35886406b6a7e19e94a9693e66acd93559357405
Pol22 <pashazor22@gmail.com>
2021-12-14 06:56:59 +0300

    Updated to_pics.py

--- to_pics.py
+++ to_pics.py
@@ -16,9 +16,7 @@ def warp_flow(img, flow):
 
 def main():
     parser = argparse.ArgumentParser(description='Dataset creator')
-    parser.add_argument('--lr_video', help='Low resolution MP4 Video file',
-                        required=True)
-    parser.add_argument('--hr_video', help='High resolution MP4 Video file',
+    parser.add_argument('--video', help='MP4 Video file',
                         required=True)
     parser.add_argument('--dir', help='Output dir', default='frames')
     args = parser.parse_args()
@@ -26,8 +24,7 @@ def main():
     if not os.path.exists(args.dir):
         os.mkdir(args.dir)
 
-    cap = cv2.VideoCapture(args.lr_video)
-    res_cap = cv2.VideoCapture(args.hr_video)
+    cap = cv2.VideoCapture(args.video)
     fps = cap.get(cv2.CAP_PROP_FPS)
     frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     print(f'FPS {fps}')
@@ -35,18 +32,27 @@ def main():
 
     inst = cv2.DISOpticalFlow.create(cv2.DISOPTICAL_FLOW_PRESET_MEDIUM)
     inst.setUseSpatialPropagation(False)
+
     _, prev = cap.read()
-    _, frame = cap.read()
-    _, _ = res_cap.read()
+    _, res_frame = cap.read()
+
+    prev = cv2.resize(
+        prev, (0,0), fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR)
+    frame = cv2.resize(
+        res_frame, (0,0), fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR)
+
     prev_gray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     nxt = None
     flow = None
 
     for i in tqdm(range(2, frames)):
-        # Capture frame-by-frame
-        _, nxt = cap.read()
-        _, res_frame = res_cap.read()
+        cv2.imwrite(os.path.join(args.dir, f'{i-1}_frame.png'), frame)
+        cv2.imwrite(os.path.join(args.dir, f'{i-1}_res.png'), res_frame)
+
+        _, res_frame = cap.read()
+        nxt = cv2.resize(
+            res_frame, (0,0), fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR)
 
         nxt_gray = cv2.cvtColor(nxt, cv2.COLOR_BGR2GRAY)
 
@@ -55,20 +61,15 @@ def main():
         flow = inst.calc(nxt_gray, frame_gray, None)
         warpped_nxt = warp_flow(nxt, flow)
 
-        cv2.imwrite(os.path.join(args.dir, f'{i-1}_frame.png'), frame)
         cv2.imwrite(os.path.join(args.dir, f'{i-1}_prev.png'), warpped_prev)
         cv2.imwrite(os.path.join(args.dir, f'{i-1}_nxt.png'), warpped_nxt)
-
-        cv2.imwrite(os.path.join(args.dir, f'{i-1}_res.png'), res_frame)
 
         prev = frame
         prev_gray = frame_gray
         frame = nxt
         frame_gray = nxt_gray
 
-    # When everything done, release the video capture object
     cap.release()
-    res_cap.release()
 
 
 if __name__ == '__main__':
